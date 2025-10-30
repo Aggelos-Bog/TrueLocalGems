03ae123b97fb3b8b558b13ee7901647df3bc5c27
Aggelos-Bog <aggelosvougiouklakhs@gmail.com>
2025-10-30 21:39:44 +0200

    chore: initial commit (vue + express + vuetify setup)

---
+++ frontend/vue-frontend/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="text-center">
+        <v-btn color="primary">Hello Vuetify</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
